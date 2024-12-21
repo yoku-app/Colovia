6ef9ed4cf07b18b6fe515fee78fa91e21ca893c2
Jared Tucker <jtuc0006@student.monash.edu>
2024-12-21 20:37:46 +1100

    Init Core Structure

---
+++ user-service/src/test/java/com/yoku/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoku.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
