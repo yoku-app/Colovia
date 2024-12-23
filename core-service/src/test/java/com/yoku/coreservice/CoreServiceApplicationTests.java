c928dcda218cdd89b05187c33fe08af11422ea5c
Jared Tucker <jtuc0006@student.monash.edu>
2024-12-23 18:43:43 +1100

    Server Restructure

--- core-service/src/test/java/com/yoku/coreservice/CoreServiceApplicationTests.java
+++ core-service/src/test/java/com/yoku/coreservice/CoreServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.yoku.userservice;
+package com.yoku.coreservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserServiceApplicationTests {
+class CoreServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
