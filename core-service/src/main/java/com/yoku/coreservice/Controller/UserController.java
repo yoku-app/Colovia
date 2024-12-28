58dcc5a3d429005c7ceb79752009eeff7057fe15
Jared Tucker <jtuc0006@student.monash.edu>
2024-12-28 16:46:57 +1100

    Init Server Structure

---
+++ core-service/src/main/java/com/yoku/coreservice/Controller/UserController.java
@@ -0,0 +1,23 @@
+package com.yoku.coreservice.Controller;
+
+import com.yoku.coreservice.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<> registerUser(@RequestBody) {
+        userService.registerUser();
+    }
+
+}
---
+++ core-service/src/main/java/com/yoku/coreservice/Entity/UserProfile.java
@@ -0,0 +1,9 @@
+package com.yoku.coreservice.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user_profiles")
+public class UserProfile {
+}
