8b8372bcd5fb84ed74bea8c5e5109904b852130a
Jared Tucker <jtuc0006@student.monash.edu>
2025-01-27 12:44:11 +1100

    Minor Alterations

--- src/test/java/com/yoku/colovia/CoreServiceApplicationTests.java
+++ src/test/java/com/yoku/colovia/CoreServiceApplicationTests.java
@@ -1,12 +1,11 @@
 package com.yoku.colovia;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+
 class CoreServiceApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
